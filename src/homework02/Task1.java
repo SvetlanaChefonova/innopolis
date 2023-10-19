f6e198161995e92ebd070a863ba10b7b4571c09a
Svetlana Chefonova <svetlana_chefonova@mail.ru>
2023-10-19 21:59:03 +0300

    Точка входа в программу. JVM Структура

---
+++ src/homework02/Task1.java
@@ -0,0 +1,13 @@
+package homework02;
+import java.util.Scanner;
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение в градусах Фаренгейта: ");
+        double fahrengeitValue = scanner.nextInt();
+        double celsiusValue = (fahrengeitValue - 32) * 5 / 9;
+        System.out.println(fahrengeitValue +" градусов по Фаренгейту ="+" "+celsiusValue +" " + "по Цельсию.");
+    }
+}
+
---
+++ src/homework02/Task2.java
@@ -0,0 +1,23 @@
+package homework02;
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое целое число: ");
+        int numberOne = scanner.nextInt();
+        System.out.println("Введите второе целое число: ");
+        int numberTwo = scanner.nextInt();
+
+        System.out.printf("Сумма двух целых чисел: %s\n", numberOne + numberTwo);
+        System.out.printf("Разница двух целых чисел: %s\n", numberOne - numberTwo);
+        System.out.printf("Произведение двух целых чисел: %s\n", numberOne * numberTwo);
+        System.out.printf("Среднее из двух целых чисел: %s\n", (numberOne + numberTwo)/2);
+        System.out.printf("Расстояние двух целых чисел: %s\n", Math.abs(numberOne - numberTwo));
+        System.out.printf("Максимальное целое число: %s\n", Math.max(numberOne, numberTwo));
+        System.out.printf("Минимальное целое число: %s\n", Math.min(numberOne,numberTwo));
+
+
+
+
+    }
+}
