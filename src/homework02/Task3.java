2c36be2665a6089afc5b5b4ef7d5b80c4aa87db3
SvetlanaChefonova <svetlana_chefonova@mail.ru>
2023-11-05 22:19:24 +0300

    Добавила задачу 3 из homework02

---
+++ src/homework02/Task3.java
@@ -0,0 +1,8 @@
+package homework02;
+
+public class Task3 {
+    public static void main(String[] args){
+        for(int i = 0; i<8;i++)
+            System.out.print("Java");
+    }
+}
---
+++ src/homework03/Task1.java
@@ -0,0 +1,16 @@
+package homework03;
+
+
+
+//public class Task1 {
+  //  public static void main(String[] args){
+       /* Televisor televisor1 = new Televisor();
+        televisor1.name = "Samsung";
+        televisor1.price = 20000;
+
+         class Televisor {
+            String name;
+            int price;
+        }
+}
+}*/
